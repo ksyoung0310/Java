e2947d852f4b4f4977d0c47b5a01124320a802f7
KSY <tjrduddl0310@gmail.com>
2024-01-25 19:21:28 +0900

    Java commit

---
+++ Java1/src/Chapter01/EX05.java
@@ -0,0 +1,10 @@
+package Chapter01;
+
+public class EX05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java1/src/Chapter02/QUIZ03.java
@@ -0,0 +1,10 @@
+package Chapter02;
+
+public class QUIZ03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
