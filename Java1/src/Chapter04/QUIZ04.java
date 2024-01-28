38a93de23c2b2338999210d3459786611484f16b
KSY <tjrduddl0310@gmail.com>
2024-01-28 22:05:28 +0900

    commit

--- Java1/src/Chapter04/QUIZ04.java
+++ Java1/src/Chapter04/QUIZ04.java
@@ -7,10 +7,16 @@ public class QUIZ04 {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		System.out.print("년도를 입력하세요 >> ");
-		int year = 0;
-		if (year % 4 == 0) {
-			
-		}
+		int year = input.nextInt();
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + "은 윤년입니다.");
+        } 
+		else if (year % 4 == 0 && year % 100 == 0) {
+            System.out.println(year + "은 윤년이 아닙니다.");
+        } 
+		else {
+            System.out.println(year + "은 윤년이 아닙니다.");
+        }
 		
 	}	
 
