c2005610e488891c39ac8e4fbf473267c3c14390
KSY <tjrduddl0310@gmail.com>
2024-01-28 21:43:41 +0900

    commit

--- Java1/src/Chapter04/QUIZ04.java
+++ Java1/src/Chapter04/QUIZ04.java
@@ -1,10 +1,17 @@
 package Chapter04;
+import java.util.Scanner;
 
 public class QUIZ04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		Scanner input = new Scanner(System.in);
+		System.out.print("년도를 입력하세요 >> ");
+		int year = 0;
+		if (year % 4 == 0) {
+			
+		}
+		
+	}	
 
 }
